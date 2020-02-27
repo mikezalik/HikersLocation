fc4acb78e58b7ddd9d253c68b93f15b8a5aceeae
mikezalik <mike.zalik@gmail.com>
2020-02-27 15:23:27 -0800

    Removes unused import statements

--- app/src/main/java/com/mikezalik/hikerslocation/MainActivity.java
+++ app/src/main/java/com/mikezalik/hikerslocation/MainActivity.java
@@ -14,7 +14,6 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import java.util.List;
